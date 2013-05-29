b2d1efc8448938b7b07be01889ba1a4669d15053
John Maddux <jemaddux@gmail.com>
2013-05-29 15:34:14 -0600

    FIRST

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crafty::Application.config.secret_token = '187c6212ec47c8bbab05b8983705ce9496e801723f0786a82e342fe88d3aee8d122f963263a126bdf7863dbf73b8dc90c0748a8ba5bcf4cf404ac6cf4ff172d9'
