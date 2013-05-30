a6f5a7fc9cc006f8e5cc98965d42876e67358c5f
John Maddux <jemaddux@gmail.com>
2013-05-30 11:49:33 -0600

    Add two controller methods for etsy in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,21 @@ class ApplicationController < ActionController::Base
   def home
 
   end
+
+  def etsy_callback
+    access_token = Etsy.access_token(
+    session[:request_token],
+    session[:request_secret],
+    params[:oauth_verifier]
+    )
+    # access_token.token and access_token.secret can now be saved for future API calls
+    redirect root_path
+  end
+
+  def login_to_etsy
+    request_token = Etsy.request_token
+    session[:request_token]  = request_token.token
+    session[:request_secret] = request_token.secret
+    redirect Etsy.verification_url
+  end
 end
