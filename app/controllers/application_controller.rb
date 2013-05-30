b2a6a3bc1f191e07e027bc1dd25627eca9be716b
John Maddux <jemaddux@gmail.com>
2013-05-30 15:14:58 -0600

    listing the users favorites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def home
-    @user = Etsy.user('johnmaddux')
-    @favs = Etsy::FavoriteListing.find_all_user_favorite_listings(1910)
+    initialize_etsy
+    @user = Etsy::User.find('johnmaddux')
+    @favs = Etsy::FavoriteListing.find_all_user_favorite_listings(@user.id)
+  end
+
+private
+  def initialize_etsy
+    Etsy.api_key = 'zq28pq66whj2cp3tfy9lqz6x'
+    Etsy.api_secret = 'owztxt2cyb'
+    Etsy.environment = :production
   end
 
   # def etsy_callback
