12d2a1c33bee7cc7684e2b93873a32ddefea02e5
John Maddux <jemaddux@gmail.com>
2013-06-01 18:30:13 -0600

    Did stuff to Polls

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
   attr_accessible :name, :rating
+  belongs_to :poll
+  has_many :item_images, :dependent => :destroy
 end
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,4 @@
 class Poll < ActiveRecord::Base
   attr_accessible :etsy_username, :name
-
+  has_many :items, :dependent => :destroy
 end
