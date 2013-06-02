f361d8cf61703985d8296cf82ecddf28a7c211df
John Maddux <jemaddux@gmail.com>
2013-06-02 17:27:00 -0600

    bam

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,8 @@ class Item < ActiveRecord::Base
   has_many :item_image_urls, :dependent => :destroy
 
   def self.create_etsy_item(etsy_item, poll_id)
-    unless Item.where(listing_id: etsy_item.result["listing_id"],
-                      poll_id: poll_id)
+    # unless Item.where(listing_id: etsy_item.result["listing_id"],
+                      # poll_id: poll_id)
       item = Item.new
       item.poll_id = poll_id
       item.name = etsy_item.result["title"]
@@ -15,6 +15,6 @@ class Item < ActiveRecord::Base
       item.save!
 
       ItemImageUrl.create_from_etsy(item.listing_id, item.id)
-    end
+    # end
   end
 end
