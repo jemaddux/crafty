f32f571304ec9801224281d610e276b8f7e0d35c
John Maddux <jemaddux@gmail.com>
2013-06-02 18:31:47 -0600

    polls created from a username

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,15 @@
 class Poll < ActiveRecord::Base
   attr_accessible :etsy_username, :name
   has_many :items, :dependent => :destroy
+
+  def etsy
+    @user = Etsy::User.find(etsy_username)
+    @faves = Etsy::FavoriteListing.find_all_user_favorite_listings(@user.id)
+    @items = []
+    @faves.each do |fav|
+      item = Etsy::Listing.find(fav.result["listing_id"])
+      Item.create_etsy_item(item, id)
+      @items << item
+    end
+  end
 end
