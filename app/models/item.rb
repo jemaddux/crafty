e8831c260d50f4507fb494b475632630c5faf914
John Maddux <jemaddux@gmail.com>
2013-06-02 18:24:29 -0600

    bump

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,17 +4,14 @@ class Item < ActiveRecord::Base
   has_many :item_image_urls, :dependent => :destroy
 
   def self.create_etsy_item(etsy_item, poll_id)
-    # unless Item.where(listing_id: etsy_item.result["listing_id"],
-                      # poll_id: poll_id)
-      item = Item.new
-      item.poll_id = poll_id
-      item.name = etsy_item.result["title"]
-      item.rating = 0
-      item.listing_id = etsy_item.result["listing_id"]
-      item.description = etsy_item.result["description"]
-      item.save!
+    item = Item.new
+    item.poll_id = poll_id
+    item.name = etsy_item.result["title"]
+    item.rating = 0
+    item.listing_id = etsy_item.result["listing_id"]
+    item.description = etsy_item.result["description"]
+    item.save!
 
-      ItemImageUrl.create_from_etsy(item.listing_id, item.id)
-    # end
+    ItemImageUrl.create_from_etsy(item.listing_id, item.id)
   end
 end
