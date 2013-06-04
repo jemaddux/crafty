172fb95cf66b8079a898245339fbc79815835599
John Maddux <jemaddux@gmail.com>
2013-06-03 18:43:21 -0600

    jquery

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,20 @@
 class ItemsController < ApplicationController
   def rate_item
     item = Item.find(params[:item_id])
-    item.rating = params[:rating]
+    if params[:rating].to_f <= 5 && params[:rating].to_f >= 0
+      rating = params[:rating].to_f
+    else
+      rating = item.rating
+    end
+    total_rating = item.rating * item.times_rated
+    total_rating += rating
+    item.times_rated += 1
+    item.rating = total_rating / item.times_rated
     item.save!
-    redirect_to :back
+  end
+
+  def item_rating
+    item = Item.find(params[:item_id])
+    item.rating
   end
 end
