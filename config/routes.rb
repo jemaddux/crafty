5c229cc1675b7c4b40b7c7a763ae909d2e43fe2e
John Maddux <jemaddux@gmail.com>
2013-06-03 21:56:12 -0600

    items can be rated now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Crafty::Application.routes.draw do
   resources :polls
+  match "item_rating", to: "items#item_rating"
   match "rate_item", to: "items#rate_item"
   match "etsy_login", to: "application#etsy_login"
   match "etsy_callback", to: "application#etsy_callback"
