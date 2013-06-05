17c108fc3635875389d43865a3eb38fdea8e2ce5
John Maddux <jemaddux@gmail.com>
2013-06-05 14:41:14 -0600

    app worksgit add -Agit add -A

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Crafty::Application.routes.draw do
   resources :polls
   match "polls/:id/select_items", to: "polls#select_items"
-  match "polls/:id/select", to: "polls#select"
+  match "polls/:id/select", to: "polls#select", as: "select"
   match "results/:id", to: "polls#results", as: "results"
   match "item_rating", to: "items#item_rating"
   match "rate_item", to: "items#rate_item"
